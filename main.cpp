3a9bc4628f97d39e74cb048c9f4af58567c387ae
cbgaors120 <bgaor@students.avc.edu>
2026-03-18 22:56:37 -0700

    A44 Assignment

--- main.cpp
+++ main.cpp
@@ -5,12 +5,38 @@ using namespace std;
 int main()
 {
        int selection;
-        cout << "Which formula do you want to see?\n\n";
+        cout << "Which formula do you want to see?\n";
         cout << "1. Apple\n";
         cout << "2. Bannana\n";
         cout << "3. Orange\n" ;
         cout << "4. None of them\n";
         cin >> selection;
 // TODO
+switch (selection){
+        case 1:
+
+                cout << "Menu 1 selected." << endl;
+                break;
+
+        case 2:
+
+                cout << "Menu 2, Bannana selected." << endl;
+                break;
+
+        case 3:
+
+                cout << "Menu 3, Orange selected." << endl;
+                break;
+
+        case 4:
+
+                cout << "Menu 4, None, selected." << endl;
+                break;
+        
+        default:
+
+                cout << "Menu item does not exist." << endl;
+                return 1;
+}
 // TODO
 }
